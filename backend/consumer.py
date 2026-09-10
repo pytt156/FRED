d9f17d7dbd49d7bf667286d62b05cb49a96ee85b
Daniela Algerydh <danielaalgerydh@gmail.com>
2026-09-10 09:56:15 +0200

    feat: add mqtt simulation and consumer flow

--- backend/consumer.py
+++ backend/consumer.py
@@ -7,12 +7,13 @@ def on_message(client, userdata, message):
     payload = message.payload.decode()
     data = json.loads(payload)
 
-    print(data)
+    print(message.topic, data)
 
 
 if __name__ == "__main__":
     client = mqtt.Client()
     client.connect("localhost", 1883)
     client.subscribe("fred/room/metrics")
+    client.subscribe("fred/network/status")
     client.on_message = on_message
     client.loop_forever()
