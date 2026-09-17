434b8526db6eb0800a9c437906047440efd8fb7d
Ömer Aytug <aytugomer00@gmail.com>
2026-09-18 01:22:17 +0300

    chore: switch edge device config to the Azure deployment

--- edge/device_config.py
+++ edge/device_config.py
@@ -2,10 +2,10 @@
 
 # "local" - Connect to the local Docker Compose MQTT broker
 # "azure" - Connect to the Azure MQTT broker
-DEPLOYMENT = "local"
+DEPLOYMENT = "azure"
 
 LOCAL_MQTT_HOST = "192.168.1.10"
 LOCAL_MQTT_PORT = 1883
 
-AZURE_MQTT_HOST = ""
+AZURE_MQTT_HOST = "fred-mosquitto.bravebay-c1aaf610.polandcentral.azurecontainerapps.io"
 AZURE_MQTT_PORT = 8883
